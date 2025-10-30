e7629ba07975ac8181b94a644c89cd7490a2426c
Obed704 <ericgob7@gmail.com>
2025-10-30 15:19:17 +0000

    Add backend files

---
+++ middleware/outhMidleware.js
@@ -0,0 +1,19 @@
+// backend/middleware/authMiddleware.js
+import jwt from "jsonwebtoken";
+
+export const protectAdmin = (req, res, next) => {
+  try {
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token) return res.status(401).json({ error: "Not authorized" });
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    if (!decoded.isAdmin) {
+      return res.status(403).json({ error: "Admin access only" });
+    }
+
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({ error: "Not authorized" });
+  }
+};
